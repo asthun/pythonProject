d55b2a2f2e67628ba3a42deb46367ee4bdd0ce68
asthun <asthun@gmail.com>
2023-07-01 13:33:49 +0200

    test.py add

--- RedRover/Lection 6/test.py
+++ RedRover/Lection 6/test.py
@@ -1,6 +1,7 @@
 import time
 from selenium.webdriver.common.by import By
 import pytest
+import webdriver_manager
 from selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.chrome.service import Service
