5f2c8d34c7ea96377e5c649fa93b06285f8189a4
asthun <asthun@gmail.com>
2023-07-19 13:00:17 +0200

    test.py for L7 upd

--- RedRover/Lection 7/test_openweather.py
+++ RedRover/Lection 7/test_openweather.py
@@ -2,6 +2,9 @@ import time
 
 from selenium import webdriver
 import pytest
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
@@ -26,12 +29,20 @@ def test_input_search_field():
     #driver.implicitly_wait(10)
     search_city_field = driver.find_element(By.CSS_SELECTOR, "input[placeholder='Search city']")
     search_city_field.send_keys("New York")
-    search_option = driver.find_element(By.CSS_SELECTOR, "ul.search-dropdown-menu li:nth-child(1) span:nth-child(1)']")
-    search_option.click()
-
-    search_button = driver.find_element(By.CSS_SELECTOR, "button[class='button-round dark']")
+    #driver.implicitly_wait(10)
+    #search_button = driver.find_element(By.CSS_SELECTOR, "button[class='button-round dark']")
+    driver.implicitly_wait(10)
+    time.sleep(10)
+    search_button = WebDriverWait(driver, 15).until(EC.element_to_be_clickable(
+        (By.CSS_SELECTOR, "button[class='button-round dark']")))
     search_button.click()
+    time.sleep(10)
+    driver.implicitly_wait(10)
+    search_option = WebDriverWait(driver, 15).until(EC.element_to_be_clickable(
+        (By.CSS_SELECTOR, "ul.search-dropdown-menu li:nth-child(1) span:nth-child(1)")))
+    search_option.click()
     #time.sleep(10)
     driver.implicitly_wait(10)
+    time.sleep(10)
     displayed_text = driver.find_element(By.XPATH, '//*[@id="weather-widget"]//h2')
-    assert displayed_text == 'New York City, US'
+    assert displayed_text.text == 'New York City, US'
